8aa9790828d2b3bbdea9044738c1e76d762436d6
Cash <32471210+pickfr@users.noreply.github.com>
2023-08-01 16:28:00 +0100

    Add one pixel to begin testing colouring

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // Start with a blank pixel grid of 16x16
 // get cursor position and when clicked if the mouse hovers over a section colour that section in.
 
-
+// Update to new Array(len).fill(0) when done
     const pixelArray = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
                         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
                         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
@@ -20,4 +20,18 @@
                         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
                         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]];
 
+
+// create 16x16 divs that the array will map to.
+
+const containerDiv = document.getElementById("etch-a-sketch-container")
+console.log(containerDiv);
+
+// Use this to make a 16 x 16 group of divs probs with a loop
+const pixels = document.createElement(`div`);
+
+
+pixels.classList.add("pixel")
+// Put the pixels in
+containerDiv.appendChild(pixels)
+console.log(pixels);
 
