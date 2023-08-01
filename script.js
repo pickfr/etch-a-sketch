0bd99ef207d88933ed83650397fb43d4d5121985
Cash <32471210+pickfr@users.noreply.github.com>
2023-08-01 17:57:13 +0100

    Add buildpixelDivs method for making the canvas
    Array matches canvas co-ordinates

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 
 // Update to new Array(len).fill(0) when done
     const pixelArray = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2],
                         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
                         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
                         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
@@ -21,17 +21,58 @@
                         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]];
 
 
-// create 16x16 divs that the array will map to.
+                        let pixelListener;
+    
 
-const containerDiv = document.getElementById("etch-a-sketch-container")
-console.log(containerDiv);
+// create 16x16 divs that the array will map to.
+buildPixelDivs();
+
+
+function buildPixelDivs(){
+
+    const containerDiv = document.getElementById("etch-a-sketch-container");
+
+    for (let y = 0; y < pixelArray.length; y++) {
+
+        let pixelDivRow = document.createElement(`div`);
+        pixelDivRow.classList.add(`row`);
+        pixelDivRow.classList.add(`row-${y}`);
+        containerDiv.appendChild(pixelDivRow)
+
+
+
+        for (let x = 0; x < pixelArray[y].length; x++) {
+        
+            pixelArray[y][x] = document.createElement(`div`);
+            pixelArray[y][x].classList.add(`y${y}x${x}`);
+            pixelArray[y][x].classList.add(`pixel`);
+            pixelDivRow.appendChild(pixelArray[y][x]);
+            pixelArray[y][x].textContent = `[x]`;
+            pixelArray[y][x].addEventListener("mouseover",alterPixel);
+        }
+    }
+}
+
+
+
+function alterPixel(e){
+
+
+    console.log(e.target);
+    e.target.textContent = `[o]`;
+
+
+}
+
+
+
+
 
 // Use this to make a 16 x 16 group of divs probs with a loop
-const pixels = document.createElement(`div`);
+//const pixels = document.createElement(`div`);
 
 
-pixels.classList.add("pixel")
-// Put the pixels in
-containerDiv.appendChild(pixels)
-console.log(pixels);
+//pixels.classList.add("pixels")
+// Put the pixels inside the container div.
+//containerDiv.appendChild(pixels)
 
